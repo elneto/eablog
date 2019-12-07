e7d5e3ea93aa636dee21408cff0b9c3f7d856c04
neto <yo@ernestoaraiza.com>
2019-12-07 11:49:41 -0500

    ordering posts by date

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,94 +1,94 @@
-import React from 'react'
-import { graphql } from 'gatsby'
-import Img from 'gatsby-image'
-import styled from 'styled-components'
-import { animated, useSpring, config } from 'react-spring'
-import Layout from '../components/layout'
-import GridItem from '../components/grid-item'
-import SEO from '../components/SEO'
-import { ChildImageSharp } from '../types'
+import React from "react";
+import { graphql } from "gatsby";
+import Img from "gatsby-image";
+import styled from "styled-components";
+import { animated, useSpring, config } from "react-spring";
+import Layout from "../components/layout";
+import GridItem from "../components/grid-item";
+import SEO from "../components/SEO";
+import { ChildImageSharp } from "../types";
 
 type PageProps = {
   data: {
     firstProject: {
-      title: string
-      slug: string
-      cover: ChildImageSharp
-    }
+      title: string;
+      slug: string;
+      cover: ChildImageSharp;
+    };
     threeProjects: {
       nodes: {
-        title: string
-        slug: string
-        cover: ChildImageSharp
-      }[]
-    }
-    aboutUs: ChildImageSharp
-    instagram: ChildImageSharp
-  }
-}
+        title: string;
+        slug: string;
+        cover: ChildImageSharp;
+      }[];
+    };
+    aboutUs: ChildImageSharp;
+    instagram: ChildImageSharp;
+  };
+};
 
 const Area = styled(animated.div)`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   grid-template-rows: 35vw 40vw 25vw;
-  grid-template-areas:    
-    'three-projects three-projects three-projects'
-    'first-project about-us about-us'
-    'instagram instagram instagram';
+  grid-template-areas:
+    "three-projects three-projects three-projects"
+    "first-project about-us about-us"
+    "instagram instagram instagram";
 
   @media (max-width: ${props => props.theme.breakpoints[3]}) {
     grid-template-columns: repeat(4, 1fr);
     grid-template-rows: 35vw 30vw 30vw 25vw;
 
-    grid-template-areas:      
-      'three-projects three-projects three-projects three-projects'
-      'three-projects three-projects three-projects three-projects'
-      'first-project first-project about-us about-us'
-      'instagram instagram instagram instagram';
+    grid-template-areas:
+      "three-projects three-projects three-projects three-projects"
+      "three-projects three-projects three-projects three-projects"
+      "first-project first-project about-us about-us"
+      "instagram instagram instagram instagram";
   }
 
   @media (max-width: ${props => props.theme.breakpoints[1]}) {
     grid-template-columns: repeat(2, 1fr);
     grid-template-rows: repeat(8, 50vw);
 
-    grid-template-areas:      
-      'three-projects three-projects'
-      'three-projects three-projects'
-      'three-projects three-projects'
-      'three-projects three-projects'
-      'three-projects three-projects'
-      'three-projects three-projects'
-      'first-project about-us'
-      'instagram instagram';
+    grid-template-areas:
+      "three-projects three-projects"
+      "three-projects three-projects"
+      "three-projects three-projects"
+      "three-projects three-projects"
+      "three-projects three-projects"
+      "three-projects three-projects"
+      "first-project about-us"
+      "instagram instagram";
   }
 
   @media (max-width: ${props => props.theme.breakpoints[0]}) {
     grid-template-columns: 1fr;
     grid-template-rows: repeat(12, 50vw);
 
-    grid-template-areas:      
-      'three-projects'
-      'three-projects'
-      'three-projects'
-      'three-projects'
-      'three-projects'
-      'three-projects'
-      'three-projects'
-      'three-projects'
-      'three-projects'
-      'first-project'
-      'about-us'
-      'instagram';
+    grid-template-areas:
+      "three-projects"
+      "three-projects"
+      "three-projects"
+      "three-projects"
+      "three-projects"
+      "three-projects"
+      "three-projects"
+      "three-projects"
+      "three-projects"
+      "first-project"
+      "about-us"
+      "instagram";
   }
-`
+`;
 
 const FirstProject = styled(GridItem)`
   grid-area: first-project;
-`
+`;
 
 const AboutUs = styled(GridItem)`
   grid-area: about-us;
-`
+`;
 
 const ThreeProjects = styled.div`
   grid-area: three-projects;
@@ -99,18 +99,20 @@ const ThreeProjects = styled.div`
     grid-template-columns: 1fr;
     grid-template-rows: auto;
   }
-`
+`;
 
 const Instagram = styled(GridItem)`
   grid-area: instagram;
-`
+`;
 
-const Index: React.FunctionComponent<PageProps> = ({ data: { firstProject, threeProjects, aboutUs, instagram } }) => {
+const Index: React.FunctionComponent<PageProps> = ({
+  data: { firstProject, threeProjects, aboutUs, instagram }
+}) => {
   const pageAnimation = useSpring({
     config: config.slow,
     from: { opacity: 0 },
-    to: { opacity: 1 },
-  })
+    to: { opacity: 1 }
+  });
 
   return (
     <Layout>
@@ -118,16 +120,25 @@ const Index: React.FunctionComponent<PageProps> = ({ data: { firstProject, three
       <Area style={pageAnimation}>
         <ThreeProjects>
           {threeProjects.nodes.map(project => (
-            <GridItem to={project.slug} key={project.slug} aria-label={`View project "${project.title}"`}>
+            <GridItem
+              to={project.slug}
+              key={project.slug}
+              aria-label={`View project "${project.title}"`}
+            >
               <Img fluid={project.cover.childImageSharp.fluid} />
               <span>{project.title}</span>
             </GridItem>
           ))}
         </ThreeProjects>
-        <FirstProject to={firstProject.slug} aria-label={`View project "${firstProject.title}"`}>
-          <Img fluid={firstProject.cover.childImageSharp.fluid} />
-          <span>{firstProject.title}</span>
-        </FirstProject>
+        {firstProject.nodes.map(project => (
+          <FirstProject
+            to={project.slug}
+            aria-label={`View project "${project.title}"`}
+          >
+            <Img fluid={project.cover.childImageSharp.fluid} />
+            <span>{project.title}</span>
+          </FirstProject>
+        ))}
         <AboutUs to="/about" aria-label="Visit my about page">
           <Img fluid={aboutUs.childImageSharp.fluid} />
           <span>About</span>
@@ -138,25 +149,17 @@ const Index: React.FunctionComponent<PageProps> = ({ data: { firstProject, three
         </Instagram>
       </Area>
     </Layout>
-  )
-}
+  );
+};
 
-export default Index
+export default Index;
 
 export const query = graphql`
   query Index {
-    firstProject: projectsYaml {
-      title
-      slug
-      cover {
-        childImageSharp {
-          fluid(quality: 95, maxWidth: 1200) {
-            ...GatsbyImageSharpFluid_withWebp
-          }
-        }
-      }
-    }
-    threeProjects: allProjectsYaml(limit: 20, skip: 1) {
+    firstProject: allProjectsYaml(
+      limit: 1
+      sort: { fields: date, order: ASC }
+    ) {
       nodes {
         title
         slug
@@ -169,14 +172,36 @@ export const query = graphql`
         }
       }
     }
-    aboutUs: file(sourceInstanceName: { eq: "images" }, name: { eq: "berlin" }) {
+    threeProjects: allProjectsYaml(
+      limit: 9
+      sort: { fields: date, order: DESC }
+    ) {
+      nodes {
+        title
+        slug
+        cover {
+          childImageSharp {
+            fluid(quality: 95, maxWidth: 1200) {
+              ...GatsbyImageSharpFluid_withWebp
+            }
+          }
+        }
+      }
+    }
+    aboutUs: file(
+      sourceInstanceName: { eq: "images" }
+      name: { eq: "berlin" }
+    ) {
       childImageSharp {
         fluid(quality: 95, maxWidth: 1200) {
           ...GatsbyImageSharpFluid_withWebp
         }
       }
     }
-    instagram: file(sourceInstanceName: { eq: "images" }, name: { eq: "instagram" }) {
+    instagram: file(
+      sourceInstanceName: { eq: "images" }
+      name: { eq: "instagram" }
+    ) {
       childImageSharp {
         fluid(quality: 95, maxWidth: 1920) {
           ...GatsbyImageSharpFluid_withWebp
@@ -184,4 +209,4 @@ export const query = graphql`
       }
     }
   }
-`
+`;
